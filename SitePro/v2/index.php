0e2c7929db989aaa8ddf2c70669889184e234f99
Martin <m.delsart67@gmail.com>
2023-10-10 12:03:52 +0200

    Save 10-10-2023 12:03

---
+++ SitePro/v2/index.php
@@ -0,0 +1,19 @@
+<?php
+    require_once('template_header.php');
+?>
+
+    <div class="conteneur">
+        <div class="bloc-texte">
+            <p>Bonjour</p>
+        </div>
+        <div class="bloc-image">
+            <img src="../Photo identité Martin Delsart - Copie.jpg" alt="Photo d'identité">
+        </div>
+    </div>
+
+    <h1>test</h1>
+    <p class="c1">Ce paragraphe centré</p>
+    <p class="c2">Ce paragraphe sera rouge</p>
+    <p class="c1 c2">Ce paragraphe sera centré et rouge</p>
+</body>
+</html>
---
+++ SitePro/v2/template_header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SitePro</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header>
+        <h1 class="titre">Martin Delsart</h1>
+        <nav class="menu">
+            <div>
+                <div class="menu_principal">
+                    <div><a href="index.html">Accueil</a></div>
+                    <div><a href="cv.html">CV</a></div>
+                    <div><a href="projects.html">Projects</a></div>
+                </div>
+            </div>
+        </nav>
+    </header>
+
