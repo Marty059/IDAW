f5fea7d4ef69f1fd1800264859f03003a5b72fa7
Martin <m.delsart67@gmail.com>
2023-10-10 14:53:41 +0200

    Save 10-10-2023 14:53

--- SitePro/v2/template_header.php
+++ SitePro/v2/template_header.php
@@ -1,25 +1,27 @@
+
 <!DOCTYPE html>
 <html lang="fr">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>SitePro</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>SitePro</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
 
-<body>
-    <header>
-        <h1 class="titre">Martin Delsart</h1>
-        <nav class="menu">
-            <div>
-                <div class="menu_principal">
-                    <div><a href="index.php">Accueil</a></div>
-                    <div><a href="cv.php">CV</a></div>
-                    <div><a href="projects.html">Projects</a></div>
+    <body>
+        <header>
+            <h1 class="titre">Martin Delsart</h1>
+            <nav class="menu">
+                <div>
+                    <div class="menu_principal">
+                        <div class="onglet"><a href="index.php">Accueil</a></div>
+                        <div class="onglet"><a href="cv.php">CV</a></div>
+                        <div class="onglet"><a href="hobbies.php">Hobbies</a></div>
+                    </div>
                 </div>
-            </div>
-        </nav>
-    </header>
+            </nav>
+        </header>
+
 
