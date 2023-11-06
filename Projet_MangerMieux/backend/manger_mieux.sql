474f50595c66a2cf8da7a85388b6a8988b58ea41
Martin <m.delsart67@gmail.com>
2023-11-06 16:04:07 +0100

    Save

--- Projet_MangerMieux/backend/manger_mieux.sql
+++ Projet_MangerMieux/backend/manger_mieux.sql
@@ -173,3 +173,28 @@ alter table HISTORIQUE add constraint FK_HISTORIQUE2 foreign key (ID_PLAT)
 alter table USERS add constraint FK_ASSOCIATION_SPORT foreign key (ID_PRATIQUE)
       references PRATIQUE_SPORTIVE (ID_PRATIQUE) on delete restrict on update restrict;
 
+--création nutriment--
+INSERT INTO `nutriments` (`ID_NUTRIMENT`, `NOM_NUTRIMENT`) VALUES
+(6, 'calcium'),
+(3, 'fat'),
+(4, 'fiber'),
+(7, 'iron'),
+(10, 'potassium'),
+(1, 'proteins'),
+(2, 'sugars'),
+(8, 'vitamin-a'),
+(5, 'vitamin-c'),
+(9, 'vitamin-d');
+--insertion type d'aliment--
+INSERT INTO `type_aliment` (`ID_TYPE`, `NOM_TYPE`) VALUES
+(1, 'unsweetened-beverages'),
+(2, 'vegetables'),
+(3, 'cereals'),
+(4, 'poultry');
+--insertion aliment--
+INSERT INTO `aliments` (`ID_ALIMENT`, `ID_TYPE`, `NOM_ALIMENT`, `Kcal`) VALUES
+(1, 4, 'Poulet', 106);
+--composition des aliments--
+INSERT INTO `composition_aliment` (`ID_ALIMENT`, `ID_NUTRIMENT`, `QUANTITE_POUR_100G`) VALUES
+(1, 1, 23),
+(1, 3, 1.5);
