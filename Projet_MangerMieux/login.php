85eb13c7aee99a9c875c0051fefab08cf5be2fc4
Martin <m.delsart67@gmail.com>
2023-11-09 16:55:26 +0100

    Save

--- Projet_MangerMieux/login.php
+++ Projet_MangerMieux/login.php
@@ -1,3 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulaire de Connexion</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> <!-- Inclure jQuery -->
+</head>
+<body>
+    <form id="loginForm">
+        <label for="login">Login :</label>
+        <input type="text" name="login" id="login" required>
+
+        <br>
+
+        <label for="password">Mot de passe :</label>
+        <input type="password" name="password" id="password" required>
+
+        <br>
+
+        <input type="submit" value="Se Connecter">
+    </form>
+
+    <div id="message"></div>
+
 <?php
         $pageToInclude = "frontend/from_session.php";
         if(is_readable($pageToInclude))
